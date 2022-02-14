c6469f0a4c9bb52d47110268804e4168bf16644e
Matteoleccardi <97913576+Matteoleccardi@users.noreply.github.com>
2022-02-14 21:35:52 +0100

    Add files via upload

--- NRMproject02.py
+++ NRMproject02.py
@@ -36,6 +36,7 @@ area = 981.405000 # km^2
 evap_rate = 0.2 # m3/s on the whole area 
 A_cs_1 = 0.2463 # m^2
 
+
 # Stakeholders data
 # Power
 power_demand = 10 # static MW
@@ -48,6 +49,7 @@ perc_75 = np.percentile(flow, 75) # static high pulse
 perc_95 = np.percentile(flow, 95) # static maximum target outflow
 m_perc_25 = moving_perc(flow, sw=45, perc=25) # seasonal low pulse 
 m_perc_75 = moving_perc(flow, sw=45, perc=75) # seasonal low pulse
+
 
 # Alternatives: reservoir models
 A0 = 1 # undammed river, no model required
@@ -97,16 +99,16 @@ A5 = reservoir(
 		initial_level = 0
 		)
 
-# EMODPS - EVOLUTIONARY MULTY OBJECTIVE DIRECT POLICY SEARCH
 
+# EMODPS - EVOLUTIONARY MULTY OBJECTIVE DIRECT POLICY SEARCH
 # Model to test
 tested_reservoir = A5
 # Population data
-N_individuals = 50
+N_individuals = 80
 # Total generations
-N_generations = 15
+N_generations = 50
 # inputs
-N_iterations = 3
+N_iterations = 4
 ext_flow = np.array([flow for i in range(N_iterations)]).flatten().copy()
 ext_rain = np.array([rain for i in range(N_iterations)]).flatten().copy()
 # objectives
@@ -114,17 +116,28 @@ indices_list=[Ipow_Avg, Ienv_high_pulses_mean]
 indices_params_list=[30, perc_75]
 indices_inputs_list=["power", "release"]
 # optimization
-indices_for_selection_list = [0, 1] # first element of "indices_list"
+indices_for_selection_list = [0, 1] # element of "indices_list" to consider in selection algorithm
 # selection strategy
 selection_type="top half" #["all","top half"]
 # mating strategy
 n_partners=2
 # mutation strategy
+mutation_prob = 0.05
 mutation_type="random"
-mutation_prob = 0.01
 mutation_variance = 0.5
 
 
+''' Run a simulation of the dam and plot results '''
+if 1:
+	# Flow and rain input can be modified at will
+	f = ext_flow.copy() + np.random.normal(0, 10, len(ext_flow))
+	f[f<0] = 0
+	r = ext_rain.copy()
+	# This simulation will leave the simulated model exactly as it was defined
+	copy.deepcopy(A5).simulate(f, r, plot=True)
+
+
+''' Run the EMODPS algorithm '''
 pop = population(
 	base_model=tested_reservoir,
 	N_individuals=N_individuals,
@@ -145,136 +158,9 @@ pop.fully_evolve(
 	mutation_type=mutation_type
 	)
 
-
+''' See results of the policy evolution '''
 plt.close('all')
 pop.plot_pareto_all_generations()
-
-
-
-
-
-
-
-quit()
-'''
-# Initial population
-N_individuals = 30
-population = []
-for i in range(N_individuals):
-	tested_reservoir.policy.uniform_shuffle_params()
-	population.append( copy.deepcopy(tested_reservoir) )
-# first run
-performance = []
-power_setpoint = 40
-for n in range(N_individuals): # Simulate individuals
-	# Simulate system
-	level = []
-	release = []
-	power = []
-	for t in range(len(ext_flow)):
-		# at time t
-		u_valve = population[n].get_policy_u()	
-		population[n].update_valve(u_valve)
-		# at time t+1
-		f = ext_flow[t]
-		r = ext_rain[t]
-		population[n].update_level(f, r)
-		level.append(population[n].level)
-		release.append(population[n].release)
-		power.append(population[n].get_power())
-	level = np.array(level)
-	release = np.array(release)
-	power = np.array(power)
-	performance.append(objective(power, power_setpoint))
-
-# selection
-rank_idx = np.argsort(performance)
-best = rank_idx[:int(N_individuals/2)]
-# mating
-new_population = []
-for i in range(3):
-	new_individual = tested_reservoir
-	new_individual.policy.update_params(population[best[i]].policy.p)
-	new_population.append( copy.deepcopy(new_individual) )
-for i in range(3, N_individuals):
-	[prob1, prob2] = mating_prob.rvs(size=2)
-	prob1 = prob1 if prob1<len(best) else prob1=len(best)
-	prob2 = prob2 if prob2<len(best) else prob2=len(best)
-	p1 = population[best[prob1]].policy.p
-	p2 = population[best[prob2]].policy.p
-	prob_exchange = np.random.random_sample((3,))
-	p_child = prob_exchange*p1 + (1-prob_exchange)*p2
-	new_individual = tested_reservoir
-	new_individual.policy.update_params(p_child)
-	new_population.append( copy.deepcopy(new_individual) )
-# mutation
-for i in range(len(new_population)):
-	if np.random.random_sample() < mutation_prob
-		new_population[i].policy.normal_shuffle_params(self, variance=mutation_valiance)
-
-# update population and repeat
-
-plt.plot(performance, '.-')
-plt.show()
-
-
-
-
-
-quit()
-'''
-# ######
-res = A5
-policy = operating_policy(h_max_flood=res.h_max_flood, h_ds = 5, p=np.array([68, 0.5, 5+68+2]))
-res.policy = policy
-
-level = []
-release = []
-power = []
-N = 4
-block=True
-for j in range(N):
-	for t in range(-1, len(flow)-1):
-		# at time t
-		if block:
-			u_valve=0
-			if res.level >res.h_max_flood - 10 : block=False
-		else:
-			u_valve = res.get_policy_u()
-			
-		res.update_valve(u_valve)
-		# at time t+1
-		res.update_level(flow[t+1], rain[t+1])
-		level.append(res.level)
-		release.append(res.release)
-		power.append(res.get_power())
-
-level = np.array(level)
-release = np.array(release)
-power = np.array(power)
-
-
-fig, ax = plt.subplots(3)
-ax[0].plot(level, label="level")
-ax[0].plot([0, len(flow)*N], [res.h_max_dam, res.h_max_dam], "r--", alpha=0.8, label="Dam height")
-ax[0].plot([0, len(flow)*N], [res.h_max_flood-2, res.h_max_flood-2], "g--", alpha=0.8, label="Target height")
-ax[0].plot([0, len(flow)*N], [res.h_max_flood, res.h_max_flood], "--", c="orange", alpha=0.8, label="Flood line height")
-ax[0].set_xlabel("Water level [m]")
-ax[0].legend()
-ax[0].grid()
-ax[1].plot(release, "b", label="release")
-ax[1].plot([0, len(flow)*N], [perc_25, perc_25], "--", c="orange", alpha=0.8, label="25th percentile")
-ax[1].plot([0, len(flow)*N], [perc_75, perc_75], "r--", alpha=0.8, label="75th percentile")
-ax[1].set_xlabel("Water release [$m^3/s$]")
-ax[1].legend()
-ax[1].grid()
-ax[2].plot(power, "y", label="Power MW")
-ax[2].plot(np.array([m_power_demand[:-1] for j in range(N)]).flatten(), "--",c="orange", alpha=0.8, label="Minimum power requirement")
-ax[2].set_xlabel("Net output power [MW]")
-ax[2].legend()
-ax[2].grid()
-plt.show()
-
-print("Power index: ", Ipow_reliability(power[-7300:], m_power_demand) )
-
-
+# Simulate one of the results of the optimization
+pop.population[0].reset_initial_conditions()
+pop.population[0].simulate(ext_flow, ext_rain, plot=True)
--- NRMproject_utils3_reservoir.py
+++ NRMproject_utils3_reservoir.py
@@ -22,17 +22,31 @@ class reservoir():
 		self.active_capacity = self.surface_area * (self.h_max_flood - self.h_min)
 		self.flood_capacity  = self.surface_area * (self.h_max_dam - self.h_max_flood)
 		self.capacity = self.surface_area * self.h_max_dam
+		# Initial conditions
+		self.level0 = initial_level # m
+		self.release_cs0 = self.release_cs_max # m2
+		self.release0 = 0 # m3/s
+		self.overtop0 = 0
+		self.v_out0 = 0 # m/s
 		# State of the reservoir system at time t
-		self.level = initial_level # m
-		self.release_cs = self.release_cs_max # m2
-		self.release = 0 # m3/s
-		self.overtop = 0
-		self.v_out = 0 # m/s
+		self.level = self.level0
+		self.release_cs = self.release_cs0
+		self.release = self.release0
+		self.overtop = self.overtop0
+		self.v_out = self.v_out0
 		self.update_valve()
 		# Utilities
 		self.days_to_seconds = 24*60*60
 		# Operating policy related to the reservoir
 		self.policy = operating_policy(self.h_max_flood, self.h_min, p=policy_params)
+
+	def reset_initial_conditions(self):
+		self.level = self.level0
+		self.release_cs = self.release_cs0
+		self.release = self.release0
+		self.overtop = self.overtop0
+		self.v_out = self.v_out0
+		self.update_valve()
 
 	def net_inflow(self, inflow, rain):
 		rain = 0.001 * rain * self.surface_area / (self.days_to_seconds) # from mm/d to m3/s
@@ -53,7 +67,10 @@ class reservoir():
 				self.release += 100*(self.overtop) + 10*(self.overtop)**2
 
 	def set_release(self, target_release):
-		return 17.0
+		''' Function to set the target release,
+			changes the value of the valves accordingly
+		'''
+		return 0.5
 
 	def update_level(self, inflow, rain):
 		delta_level = (self.net_inflow(inflow, rain) - self.release) / self.surface_area
@@ -69,6 +86,71 @@ class reservoir():
 	def get_policy_u(self):
 		return self.policy.get_u(self.level)
 
+	def simulate(self, flow, rain, plot=False):
+		level = []
+		release = []
+		power = []
+		for t in range(len(flow)):
+			# at time t
+			u_valve = self.get_policy_u()	
+			self.update_valve(u_valve)
+			# step forward: t -> t+1
+			self.update_level(flow[t], rain[t])
+			level.append(self.level)
+			release.append(self.release)
+			power.append(self.get_power())
+		measures = {
+			"level":   np.array(level),
+			"release": np.array(release),
+			"power":   np.array(power)
+		}
+		if plot:
+			self.plot_simulation(measures, flow)
+		return measures
+
+	def plot_simulation(self, measures, inflow=None):
+		color_array = getRGBA()
+		fig, axs = plt.subplots(3)
+		# Water Level
+		axs[0].plot(measures["level"], label="Level", color=color_array[16])
+		axs[0].plot([0, len(measures["level"])], [self.h_max_dam, self.h_max_dam], "--", c=color_array[1], alpha=0.95, label="Dam height")
+		axs[0].plot([0, len(measures["level"])], [self.h_max_flood, self.h_max_flood], "--", c=color_array[19], alpha=0.8, linewidth=0.8, label="Flood threshold")
+		axs[0].plot([0, len(measures["level"])], [self.policy.h_max, self.policy.h_max], "--", c=color_array[6], alpha=0.8, linewidth=0.8, label="Policy max.")
+		axs[0].plot([0, len(measures["level"])], [self.policy.h_min, self.policy.h_min], "--", c=color_array[6], alpha=0.8, linewidth=0.8, label="Policy min.")
+		axs[0].set_ylabel("Water level (m)")
+		axs[0].grid(linestyle='--', linewidth=0.5)
+		axs[0].set_facecolor(getFC())
+		axs[0].legend()
+		# Release and natual streamflow
+		if inflow is not None:
+			axs[1].plot(inflow, label="Inflow", color=color_array[8], alpha=0.8, linewidth=0.8)
+		axs[1].plot(measures["release"], label="Release", color=color_array[18])
+		if inflow is not None:
+			p = np.percentile(inflow, 0.25)
+			axs[1].plot([0, len(measures["level"])], [p, p], "--", c="orange", alpha=0.5, linewidth=0.6, label="Inflow 25th, 50th, 75th perc.")
+			p = np.percentile(inflow, 0.50)
+			axs[1].plot([0, len(measures["level"])], [p, p], "--", c="orange", alpha=0.5, linewidth=0.6)
+			p = np.percentile(inflow, 0.75)
+			axs[1].plot([0, len(measures["level"])], [p, p], "--", c="orange", alpha=0.5, linewidth=0.6)
+			p = np.mean(inflow)
+			axs[1].plot([0, len(measures["level"])], [p, p], "--", c="red", alpha=0.5, linewidth=0.6, label="Mean")
+		axs[1].set_ylabel("Flow ($m^3/s$)")
+		axs[1].grid(linestyle='--', linewidth=0.5)
+		axs[1].set_facecolor(getFC())
+		axs[1].legend()
+		# Generated power
+		axs[2].plot(measures["power"], label="Power", color=color_array[11])
+		axs[2].set_xlabel("Simulation time (Days)")
+		axs[2].set_ylabel("Generated power (MW)")
+		axs[2].grid(linestyle='--', linewidth=0.5)
+		axs[2].set_facecolor(getFC())
+		axs[2].legend()
+		# Figure title
+		l = int( len(measures["level"])/365 )
+		fig.suptitle(f"Dam simulation over {l} years", fontsize=13)
+		# Finalise plot
+		plt.tight_layout()
+		plt.show()
 
 ### OPERATING POLICIES DEFINITION ###
 
@@ -153,22 +235,36 @@ class operating_policy():
 		colorArray = getRGBA(NumPolicies)
 		return fig, ax, colorArray
 
-	def plot_many_policies(self, ax, plotIndex, color_array=None):
+	def plot_many_policies(self, ax, plotIndex, color_array=None, pareto_indices=None, tot_policies=None):
 		if color_array is None:
 			color_ = "blue"
 		else:
 			color_ = color_array[plotIndex]
+			if pareto_indices is not None:
+				if np.sum(plotIndex == pareto_indices) != 0:
+					i = int(tot_policies * (2/20))
+					color_ = color_array[i]
+					alpha_=0.8
+				else:
+					i = int(tot_policies * (6/20))
+					color_ = color_array[i]
+					alpha_=0.4
+				cmap = matplotlib.cm.get_cmap('Spectral')
+				legend_elements = [matplotlib.lines.Line2D([0], [0], color=cmap(0.3), label='Others' ),
+		                           matplotlib.lines.Line2D([0], [0],  color=cmap(0.1), label='Pareto')
+		        ]
+				ax.legend(handles=legend_elements, loc='upper left')
 		x = np.array([self.h_min-10, self.h_min, self.p[0], self.p[2], self.h_max, self.h_max+10])
 		y = np.array([0, 0, self.p[1], self.p[1], 1, 1])
-		ax.plot(x, y, ".-", color=color_, alpha=0.5)
-		ax.plot([self.h_min, self.h_min],[-0.1, 1.1], "--", color="orange", linewidth=0.8, alpha=0.8)
-		ax.plot([self.h_max, self.h_max],[-0.1, 1.1], "--", color="orange", linewidth=0.8, alpha=0.8)
+		ax.plot(x, y, ".-", color=color_, alpha=alpha_)
+		ax.plot([self.h_min, self.h_min],[-0.1, 1.1], "--", color="orange", linewidth=0.5, alpha=alpha_)
+		ax.plot([self.h_max, self.h_max],[-0.1, 1.1], "--", color="orange", linewidth=0.5, alpha=alpha_)
 		ax.set_xlim([self.h_min-5, self.h_max+5])
 		ax.set_ylim([-0.1, 1.1])
 		ax.grid()
 		ax.set_xlabel("Reservoir level [m]")
 		ax.set_ylabel("Valve release [%]")
-		ax.set_title("Operating policy of the release valve")
+		ax.set_title(f"Operating policy of the release valve")
 		ax.set_facecolor(getFC())
 			
 	
@@ -245,7 +341,7 @@ class population():
 			# Append performances related to individual n
 			perf_list_temp = []
 			for i in range(len(self.indices_list)):
-				inp = measures[self.indices_inputs_list[i]][-365:]
+				inp = measures[self.indices_inputs_list[i]][:]
 				par = self.indices_params_list[i]
 				perform = self.indices_list[i](inp, par)
 				perf_list_temp.append( perform )
@@ -426,19 +522,21 @@ class population():
 			# Plot all policies of this generation
 			ax_pol.clear()
 			for i in range(self.N_individuals):
-				self.population[i].policy.plot_many_policies(ax_pol, plotIndex=i, color_array=col_pol)
+				self.population[i].policy.plot_many_policies(ax_pol, plotIndex=i, color_array=col_pol, pareto_indices=self.curr_pareto_idxs, tot_policies=self.N_individuals)
+			ax_pol.set_title(f"Operating policy of the release valve (Gen {g+1})")
 			# Plot performance index and pareto front of first 2 objectives
 			self.plot_pareto_synchronous(ax_par, g)
 			# Draw plots
 			plt.pause(0.5)
 			plt.draw()
 		# Last generation needs just to be tested
-		print("Generation ", self.N_generations)
+		print("Last generation ", self.N_generations)
 		self.test(flow, rain)
 		self.apply_selection(selection_type=selection_type, indices_for_selection=indices_for_selection)
 		self.gen_performance = np.array(self.gen_performance)
-		# Last plots
+		# Last plot and messages
 		plt.ioff()
+		print("Evolutionary training completed.")
 
 	def plot_pareto_synchronous(self, ax, generation_index):
 		ax.clear()
@@ -462,7 +560,7 @@ class population():
 		ymin = np.amin( self.gen_performance[:,:,1] )
 		ymax = np.amax( self.gen_performance[:,:,1] )
 		allowed_steps = np.arange(1,self.N_generations+1)
-		ax_slider = plt.axes([0.1, 0.05, 0.85, 0.02]) # left, bottom, width, height
+		ax_slider = plt.axes([0.1, 0.02, 0.8, 0.02]) # left, bottom, width, height
 		# create the sliders
 		slider_gen = Slider(
 			ax_slider, "Gen:", 1, self.N_generations,
@@ -472,7 +570,7 @@ class population():
 		slider_gen.on_changed(self.update_plot_pareto_all_generations)
 		# make first scatter
 		col_t_1   = 1.0
-		col_t     = 0.2
+		col_t     = 0.25
 		col_t_par = 0.0
 		self.scatter_ax.grid()
 		self.scatter_ax.set_xlabel("Objective 1")
@@ -480,6 +578,16 @@ class population():
 		self.scatter_ax.set_facecolor(getFC())
 		self.scatter_ax.set_xlim([xmin, xmax])
 		self.scatter_ax.set_ylim([ymin, ymax])
+		# Create custom legend
+		cmap = matplotlib.cm.get_cmap('Spectral')
+		legend_elements = [matplotlib.lines.Line2D([0], [0], marker='o', color=cmap(col_t_1), label='Previous gen.',
+                            markerfacecolor=cmap(col_t_1), markersize=5),
+                           matplotlib.lines.Line2D([0], [0], marker='o', color=cmap(col_t), label='Current gen.',
+                            markerfacecolor=cmap(col_t), markersize=5),
+                           matplotlib.lines.Line2D([0], [0], marker='o', color=cmap(col_t_par), label='Pareto (current)',
+                            markerfacecolor=cmap(col_t_par), markersize=5)
+        ]
+		self.scatter_ax.legend(handles=legend_elements, loc='upper right')
 		# Put data and colors
 		gen = self.N_generations
 		par_idxs = self.all_pareto_idxs[gen-1][:]
@@ -488,42 +596,14 @@ class population():
 			[np.repeat(col_t_1,self.N_individuals) ,np.repeat(col_t,self.N_individuals) , np.repeat(col_t_par, len(self.all_pareto_idxs[gen-1][:]))]
 		).flatten()
 		self.scatter_ax.set_title(f"Pareto frontier plot of generation {gen-1}")
-		self.scatter = self.scatter_ax.scatter(data[:,0], data[:,1], c=colors, alpha=0.9, cmap=matplotlib.cm.get_cmap('Spectral'))
+		self.scatter = self.scatter_ax.scatter(data[:,0], data[:,1], c=colors, alpha=0.9, cmap=cmap)
 		self.scatter_ax.set_title(f"Pareto frontier plot of generation {gen}")
 		self.scatter_fig.canvas.draw_idle()
-		plt.show()	
-		#
-		'''
-		plt.ion()
-		generation_index = 0
-		while generation_index < length:
-			ax.clear()
-			colArray = getRGBA()
-			if generation_index-1 >= 0:
-				ax.scatter(self.gen_performance[generation_index-1][:,0], self.gen_performance[generation_index-1][:,1], alpha=0.6, color=colArray[19], label="Gen. t-1")
-			ax.scatter(self.gen_performance[generation_index][:,0], self.gen_performance[generation_index][:,1], alpha=0.6, color=colArray[6], label="Gen. t")
-			ax.scatter(self.gen_performance[generation_index][self.curr_pareto_idxs,0], self.gen_performance[generation_index][self.curr_pareto_idxs,1], alpha=0.2, color=colArray[0], label="Pareto (Gen. t)")
-			ax.grid()
-			ax.set_xlabel("Objective 1")
-			ax.set_ylabel("Objective 2")
-			ax.set_title(f"Pareto frontier plot of generation {generation_index+1}")
-			ax.legend()
-			ax.set_facecolor(getFC())
-			ax.set_xlim([xmin, xmax])
-			ax.set_ylim([ymin, ymax])
-			plt.draw()
-			plt.pause(0.5)
-			##
-			generation_index += 1
-			if generation_index == length:
-				generation_index = 0
-		plt.ioff()
-		'''
-
-
+		plt.show()
+		
 	def update_plot_pareto_all_generations(self, gen):
 		col_t_1   = 1.0
-		col_t     = 0.2
+		col_t     = 0.25
 		col_t_par = 0.0
 		label_t_1 = "Gen. " + str(int(gen-1))
 		label_t = "Gen. " + str(int(gen))
@@ -541,7 +621,7 @@ class population():
 			colors = np.concatenate(
 				[np.repeat(col_t_1,self.N_individuals) ,np.repeat(col_t,self.N_individuals) , np.repeat(col_t_par, len(self.all_pareto_idxs[gen-1][:]))]
 			).flatten()
-			self.scatter_ax.set_title(f"Pareto frontier plot of generation {gen-1}")
+			self.scatter_ax.set_title(f"Pareto frontier plot of generation {gen}")
 		self.scatter.set_offsets(data)
 		self.scatter.set_array(colors)
 		self.scatter_fig.canvas.draw_idle()
@@ -559,38 +639,37 @@ class population():
 ''' w is water demand expressed as m3/s daily mean '''
 def Iirr_reliability(x, w):
 	out = np.sum( x >= w ) / len(x)
-	return out
+	return np.exp(-out)
 def Iirr_vulnerability(x, w, power=1):
-	num = np.sum( np.max(w - x, 0) ) ** power
+	y = w - x
+	y[y<0] = 0
+	num = np.sum( y ) ** power
 	den = np.sum( x < w )
 	if den == 0:
 		res = 0
 	else:
 		res = num / den
-	return res
+	return 1-np.exp(res)
 def Iirr_resilience(x, w):
 	den = x[:-1] < w
 	num = np.sum( den and (x[1:] >= w) )
 	den = np.sum(x[:-1] < w)
 	if den == 0:
-		res = 0
+		res = 1
 	else:
 		res = num / den
-	return res
+	return 1-res
 
 # Water supply for hydroelectric power
 def Ipow_Avg(power, lambda_=30):
 	''' power is not additive like i.e. water demand '''
 	m = np.mean(power)
 	return np.exp(-m/lambda_)
-
 def I_pow_RMSE_from_setpoint(power, power_setpoint):
 	return np.sqrt( np.mean( (power-power_setpoint)**2 ) )
-
 def Ipow_reliability(power, pow_demand):
 	out = np.sum( power >= pow_demand ) / len(power)
-	return out
-
+	return np.exp(-out)
 def Ipow_vulnerability(power, pow_demand, risk_aversion=1):
 	num = np.sum( np.max(pow_demand - power, 0) ) ** risk_aversion
 	den = np.sum( power < pow_demand )
@@ -598,36 +677,35 @@ def Ipow_vulnerability(power, pow_demand, risk_aversion=1):
 		res = 0
 	else:
 		res = num / den
-	return res
-
+	return 1-np.exp(res)
 def Ipow_resilience(power, pow_demand):
 	den = power[:-1] < pow_demand
 	num = np.sum( den and (power[1:] >= pow_demand) )
 	den = np.sum(power[:-1] < pow_demand)
 	if den == 0:
-		res = 0
+		res = 1
 	else:
 		res = num / den
-	return res
+	return 1-res
 
 # Flooding indicators
 ''' h_max is the flood threshold '''
 def Iflood_yearly_avg(h, hmax):
 	n_years = len(h)/365
 	out = np.sum( h >= hmax ) / n_years
-	return out
+	return 1-np.exp(-out)
 
 def Iflood_max_area(h, hmax):
 	steepness = 0.087 # steepness of 5 degrees
 	x = (h-hmax)/steepness
 	x_max = np.max([x, 0])
-	return x_max
+	return 1-np.exp(-x_max)
 
 def Iflood_kayakClub(h, h_active_storage):
 	h_club = h_active_storage + 3 # m
 	n_years = len(h)/365
 	out = np.sum( h >= h_active_storage ) / n_years
-	return out
+	return 1-np.exp(-out)
 
 # Environmental indicators
 def Ienv_low_pulses(release, LP):
@@ -635,16 +713,19 @@ def Ienv_low_pulses(release, LP):
 	n_years = len(release)/365
 	out = np.sum( release < LP ) / n_years
 	return out
-
 def Ienv_high_pulses(release, HP):
 	''' HP is the 75 percentile of the natural system '''
 	n_years = len(release)/365
 	out = np.sum( release > HP ) / n_years
 	return out
-
+''' For the optimization algorithm, use these two as environmental indicators, not the two above '''
+def Ienv_low_pulses_mean(release, LP, lambda_=40):
+	''' HP is the 75 percentile of the natural system '''
+	m = np.mean( release[release < LP] )
+	return 1-np.exp(-m/lambda_)
 def Ienv_high_pulses_mean(release, HP, lambda_=40):
 	''' HP is the 75 percentile of the natural system '''
-	m = np.mean( release[release> HP] )+0.001
+	m = np.mean( release[release > HP] )
 	return 1-np.exp(-m/lambda_)
 
 
